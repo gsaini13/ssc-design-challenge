f40244ab2aa1ec820751135b234795ea0bd2bc8c
Gitesh Saini <giteshsaini13@gmail.com>
2021-10-19 00:29:09 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'ssc-design-challenge';
+  status: boolean = false;
+  accordion: boolean = true;
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    if (view === 'month') {
+      const date = cellDate.getDate();
+
+      return (date === 1 || date === 20) ? 'example-custom-date-class' : '';
+    }
+
+    return '';
+  }
+
+  columnDefs = [
+    { field: 'Fund', flex: 1 },
+    { field: 'Pending Actions', flex: 1 },
+    { field: 'Record Info', flex: 1 },
+    { field: 'MTD Book P&L', flex: 1 },
+    { field: 'YTD Book P&L', flex: 1 },
+    { field: 'End Book NAV', pinned: 'right' }
+  ];
+
+  defaultColDef = {
+    editable: true,
+    filter: 'agTextColumnFilter'
+  };
+
+  headerHeight = 40;
+
+  rowData = [
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYEHUSLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMCLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYEHUSLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMCLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYEHUSLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMCLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYEHUSLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMFLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' },
+      { 'Fund': 'ARYMCLP', 'Pending Actions': 'Icon', 'Record Info': '148,579,476', 'MTD Book P&L': '148,579,476', 'YTD Book P&L': '148,579,476', 'End Book NAV': '255,080,079' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  imports: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule
+ ],
+  exports: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule { }
 
 @NgModule({
   declarations: [
@@ -10,7 +38,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([]),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
